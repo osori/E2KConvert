0d229fe1e359828bf27e88358c17fdd39c9754e0
Ilkyu Ju <ilkyu@cs.wisc.edu>
2016-02-28 19:06:15 -0600

    Create E2K.java

---
+++ E2K.java
@@ -0,0 +1,29 @@
+public class E2K {
+	public static final String e2kA = "ㅁ";
+	public static final String e2kB = "ㅠ";
+	public static final String e2kC = "ㅊ";
+	public static final String e2kD = "ㅇ";
+	public static final String e2kE = "ㄷ";
+	public static final String e2kF = "ㄹ";
+	public static final String e2kG = "ㅎ";
+	public static final String e2kH = "ㅗ";
+	public static final String e2kI = "ㅑ";
+	public static final String e2kJ = "ㅓ";
+	public static final String e2kK = "ㅏ";
+	public static final String e2kL = "ㅣ";
+	public static final String e2kM = "ㅡ";
+	public static final String e2kN = "ㅜ";
+	public static final String e2kO = "ㅐ";
+	public static final String e2kP = "ㅔ";
+	public static final String e2kQ = "ㅂ";
+	public static final String e2kR = "ㄱ";
+	public static final String e2kS = "ㄴ";
+	public static final String e2kT = "ㅅ";
+	public static final String e2kU = "ㅕ";
+	public static final String e2kV = "ㅍ";
+	public static final String e2kW = "ㅈ";
+	public static final String e2kX = "ㅌ";
+	public static final String e2kY = "ㅛ";
+	public static final String e2kZ = "ㅋ";
+
+}
